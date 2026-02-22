3218c7cd9a91d6dc87d739852b9e74ee1af5a321
0xFA99 <fajar.nur.h@pm.me>
2026-02-23 04:28:40 +0700

    refactor: simplify main.c

--- src/main.c
+++ src/main.c
@@ -9,31 +9,9 @@ int main(void)
     const int screenHeight = 600;
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
+
     InitWindow(screenWidth, screenHeight, "RPG Game");
-
-    // Item hair = LoadItem("assets/characters/ivy/ivy_hair_basic.bin", SLOT_HAIR, "hair");
-    // Item shirt = LoadItem("assets/characters/ivy/ivy_shirt_basic.bin", SLOT_TOP, "shirt");
-    // Item bottom = LoadItem("assets/characters/ivy/ivy_bottom_basic.bin", SLOT_BOTTOM, "bottom");
-
-    // GameState game = {0};
-    // GameInit(&game);
-
-    // Player player = InitPlayerAt(1, 1);
-    // PlayerEquipItem(&player, hair);
-    // PlayerEquipItem(&player, shirt);
-    // PlayerEquipItem(&player, bottom);
-
-    // Vector2 spawnPos = {0};
-    // GameLoadMap(1, &game, &spawnPos);
-    // player.movement.tilePosition = spawnPos;
-    // player.movement.position = (Vector2){ spawnPos.x * TILE_SIZE, spawnPos.y * TILE_SIZE };
-    // player.movement.targetTilePosition = spawnPos;
-    // player.movement.isMoving = false;
-
-    // GameCamera camera = InitGameCamera(screenWidth, screenHeight);
-
-    // ScreenManager sm = { SCREEN_TITLE, .data.title = NULL };
-    // ChangeToScreen(&sm, SCREEN_TITLE);
+    SetExitKey(KEY_NULL);
 
     GameState sm = {
         .gameRunning    = true,
@@ -46,7 +24,7 @@ int main(void)
             .Unload         = ScreenTitleUnload,
             .screenUpdated  = false
         },
-        .frameTime = 0.0f
+        .frameTime = 0.0f,
     };
 
     sm.currentScreen.Init(&sm.currentScreen);
@@ -54,22 +32,22 @@ int main(void)
     SetTargetFPS(60);
 
     while (!WindowShouldClose() && sm.gameRunning) {
+
+        if (IsWindowResized()) printf("RESIZE!!!!\n");
+
         sm.frameTime = GetFrameTime();
+
+        sm.currentScreen.Update(&sm, &sm.gameRunning);
 
         if (sm.currentScreen.screenUpdated) {
             UpdateScreen(&sm.currentScreen);
         }
-
-        sm.currentScreen.Update(&sm, &sm.gameRunning);
 
         BeginDrawing();
             ClearBackground(BLACK);
             sm.currentScreen.Draw(&sm.currentScreen);
         EndDrawing();
     }
-
-    // ChangeToScreen(&sm, -1);
-    // GameUnload(&game);
 
     sm.currentScreen.Unload(&sm.currentScreen);
 
